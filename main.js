b0a7ab31c8f773ee72f2880d837cc3fe21f651b3
DylonKejhu <42734487+DylonKejhu@users.noreply.github.com>
2022-11-25 16:45:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-const apiKey = 'pk.eyJ1IjoiYWxmcmVkMjAxNiIsImEiOiJja2RoMHkyd2wwdnZjMnJ0MTJwbnVmeng5In0.E4QbAFjiWLY8k3AFhDtErA';
 const mymap = L.map('map').setView([40.770116, -73.967909], 13);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+L.tileLayer('/resources/images/AureliMap.webp', {
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: apiKey
 }).addTo(mymap);
 
 // Adding Marker
